6c8d05b2ca1773be40ff378782d57926b44d39fd
lilBchii <berenice.bianchi04@gmail.com>
2026-01-18 17:30:18 +0100

    Remove useless commented code

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -3,11 +3,7 @@ use bevy::prelude::*;
 use bevy_ecs_tiled::prelude::*;
 
 use crate::car::spawn_car;
-use crate::common::{
-    AppState, LEVEL_COUNT, LVL0_P1_START, LVL1_P1_START, LVL2_P1_START, LVL3_P1_START,
-};
 
-/*
 #[derive(Component, Clone, PartialEq, Eq, Debug, Default)]
 pub enum LevelItem {
     #[default]
@@ -15,7 +11,12 @@ pub enum LevelItem {
     Road,
     Sky,
     StartingLine,
-}*/
+}
+
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Default, Reflect)]
+#[require(Transform)]
+#[reflect(Component)]
+struct SpawnPoint;
 
 // Spawn Demcity level
 pub fn spawn_demcity_level(
@@ -35,80 +36,6 @@ pub fn spawn_demcity_level(
                     .insert(RigidBody::Static);
             },
         );
-    spawn_car(&mut commands, atlas_layouts, &asset_server, 1000.0, 700.0);
+    // TODO: wait for map spawn to spawn player
+    spawn_car(&mut commands, atlas_layouts, &asset_server, 0.0, 0.0);
 }
-
-// pub fn setup_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
-//     let bg: bevy::prelude::Handle<Image> = asset_server.load("spacebg.png");
-
-//     commands.spawn(LdtkWorldBundle {
-//         ldtk_handle: asset_server.load("mapatlas.ldtk"),
-//         transform: Transform::from_xyz(0., 0., 10.),
-//         ..Default::default()
-//     });
-//     spawn_car(
-//         &mut commands,
-//         &asset_server,
-//         LVL0_P1_START.0,
-//         LVL0_P1_START.1,
-//     );
-// }
-
-// pub fn switch_level(
-//     mut commands: Commands,
-//     kb: Res<Input<KeyCode>>,
-//     mut level_selection: ResMut<LevelSelection>,
-//     q_car: Query<Entity, With<Car>>,
-//     asset_server: Res<AssetServer>,
-//     mut app_state: ResMut<NextState<AppState>>,
-// ) {
-//     if kb.just_pressed(KeyCode::Space) {
-//         if let LevelSelection::Index(index) = *level_selection {
-//             // Despawn car
-//             for car in &q_car {
-//                 commands.entity(car).despawn_recursive();
-//             }
-
-//             // Switch level
-//             if index as i32 == LEVEL_COUNT - 1 {
-//                 *level_selection = LevelSelection::Index(0);
-//                 app_state.set(AppState::StartMenu);
-//             } else {
-//                 let n_index = index + 1;
-//                 *level_selection = LevelSelection::Index(n_index);
-//                 match n_index {
-//                     1 => spawn_car(
-//                         &mut commands,
-//                         &asset_server,
-//                         LVL1_P1_START.0,
-//                         LVL1_P1_START.1,
-//                     ),
-//                     2 => spawn_car(
-//                         &mut commands,
-//                         &asset_server,
-//                         LVL2_P1_START.0,
-//                         LVL2_P1_START.1,
-//                     ),
-//                     3 => spawn_car(
-//                         &mut commands,
-//                         &asset_server,
-//                         LVL3_P1_START.0,
-//                         LVL3_P1_START.1,
-//                     ),
-//                     _ => {}
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// pub fn cleanup_ldtk_world(
-//     mut commands: Commands,
-//     q_ldtk_world: Query<Entity, With<Handle<LdtkAsset>>>,
-// ) {
-//     for entity in &q_ldtk_world {
-//         commands.entity(entity).despawn_recursive();
-//     }
-// }
-
-//pub fn reset_level_selection
