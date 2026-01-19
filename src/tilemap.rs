739919540b2aa5e781ee7f51f1ec8fc6d912fdff
lilBchii <berenice.bianchi04@gmail.com>
2026-01-19 12:49:12 +0100

    Add starting line

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -2,13 +2,22 @@ use avian2d::prelude::*;
 use bevy::prelude::*;
 use bevy_ecs_tiled::prelude::*;
 
-#[derive(Component, Clone, PartialEq, Eq, Debug, Default)]
-pub enum LevelItem {
-    #[default]
-    None,
-    Road,
-    Sky,
-    StartingLine,
+#[derive(Component, Debug, Default, Clone, Copy, PartialEq, Reflect)]
+#[reflect(Component, Default)]
+struct StartingLine;
+
+fn create_starting_line(
+    mut collider_created: On<TiledEvent<ColliderCreated>>,
+    mut commands: Commands,
+    zone_query: Query<Entity, With<StartingLine>>,
+) {
+    let evt = collider_created.event();
+    if zone_query.get(evt.event.collider_of.0).is_ok() {
+        commands
+            .entity(evt.origin)
+            .insert((Sensor, CollisionEventsEnabled));
+
+    }
 }
 
 // Spawn Demcity level
@@ -28,5 +37,6 @@ pub fn spawn_demcity_level(
                     .entity(collider_created.event().origin)
                     .insert(RigidBody::Static);
             },
-        );
+        )
+        .observe(create_starting_line);
 }
